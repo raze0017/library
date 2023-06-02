8dae2803cd65cfdd639123d4dd0f8889d786cc3e
raze0017 <appurazeem2000@gmail.com>
2023-06-02 21:26:31 +0530

    the card is not appearing and the box is not going away

---
+++ script.js
@@ -0,0 +1,94 @@
+const but=document.querySelector('.but')
+const info=document.querySelector('.added')
+const cardContainer=document.createElement('div')
+cardContainer.classList.add('card-container')
+function Book(name, author, pages){
+    this.name=name;
+    this.author=author;
+    this.pages=pages;
+}
+const library=[];
+const form=document.createElement('form')
+const nameLabel=document.createElement('label')
+nameLabel.textContent="Book:"
+const authorLabel=document.createElement('label')
+authorLabel.textContent="   Author:"
+const nameInput=document.createElement('input')
+nameInput.setAttribute('type', 'text');
+nameInput.setAttribute('name', 'book');
+const authorInput=document.createElement('input')
+authorInput.setAttribute('type' ,'text')
+authorInput.setAttribute('name', 'author')
+const pagesLabel=document.createElement('label')
+const pagesInput=document.createElement('input')
+pagesLabel.textContent="  pages:"
+pagesInput.setAttribute('type', 'number')
+pagesInput.setAttribute('name','pages')
+const textCon=document.createElement('div')
+textCon.textContent="Did you read this book?:"
+textCon.style.fontSize='12px';
+const checkBox=document.createElement('input')
+checkBox.setAttribute('type','checkbox')
+checkBox.classList.add('tick-checkbox');
+const space=document.createElement('div')
+space.style.marginTop='10px';
+const submitButton=document.createElement('button')
+submitButton.setAttribute('type', 'submit')
+submitButton.textContent="Submit"
+submitButton.addEventListener('click', function(event){
+    event.preventDefault();
+
+    const bookName=nameInput.value;
+    const bookAuthor=authorInput.value;
+    const bookPages=pagesInput.value;
+
+    const newBook=new Book(bookName,bookAuthor,bookPages);
+
+    library.push(newBook);
+
+    const card=document.createElement('div');
+    card.classList.add('card');
+    
+
+    const bookNameLabel=document.createElement('p');
+    bookNameLabel.textContent=`book: ${newBook.name}$`;
+
+    const bookAuthorLabel=document.createElement('p');  
+    bookAuthorLabel.textcontent=`Author: ${newBook.author}$`;
+
+    const bookPagesLabel=document.createElement('p');
+    bookPagesLabel.textContent=`Pages: ${newBook,pages}$`;
+
+    card.appendChild(bookNameLabel);
+    card.appendChild(bookAuthorLabel);
+    card.appendChild(bookPagesLabel);
+
+    cardContainer.appendChild(card);
+
+    nameinput.value='';
+    authorInput.value='';
+    pagesInput.value='';
+    info.removeChild(form)
+
+});
+form.appendChild(nameLabel);
+form.appendChild(nameInput);
+form.appendChild(authorLabel);
+form.appendChild(authorInput);
+form.appendChild(pagesLabel)
+form.appendChild(pagesInput);
+form.appendChild(textCon);
+form.appendChild(checkBox)
+form.appendChild(space);
+form.appendChild(submitButton);
+
+
+function displayCardContainer(){
+    info.removeChild('box');
+    info.appendChild(cardContainer)
+}
+submitButton.addEventListener('click',displayCardContainer);
+but.addEventListener('click', function(){
+    info.classList.add('box');
+    info.appendChild(form);
+    });
